2d6fd259e00c1011b8119b476e9fd535f3f49538
Florêncio Gonçalves <109610920+florencioGoncalves@users.noreply.github.com>
2023-06-30 00:15:34 +0100

    dark mode fixes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,13 @@
 // Dark mode toggle
-document.querySelector('.dark-mode-toggle').addEventListener('click', () => {
-  document.querySelector('body').classList.toggle('dark-mode');
-})
+const body = document.querySelector('body');
+const toggle = document.querySelector(".dark-mode-toggle");
+const moon = document.querySelector('.moon')
+toggle.addEventListener("click", () => {
+	if (body.classList.contains("dark-mode")) {
+		document.querySelector("body").classList.toggle("dark-mode");
+		moon.setAttribute("name", "moon-outline");
+	} else {
+		document.querySelector("body").classList.toggle("dark-mode");
+		moon.setAttribute("name", "moon");
+	}
+});
